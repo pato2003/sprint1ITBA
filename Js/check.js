62a5bd689cecac9ae70a4ee3d18d6588504a8765
pato2003 <patogabrielferreyra@gmail.com>
2024-08-28 17:20:36 -0300

    mensaje de confirmacion de operaciones añadido

--- Js/check.js
+++ Js/check.js
@@ -26,8 +26,10 @@ function acceder() {
 const contenidoPago = document.querySelector(".input-pago")
 const contenidoPrestamo = document.querySelector(".input-prestamo")
 const contenidoTransferencia = document.querySelector(".input-transferencia")
+const btnSubmit = document.querySelector("#enviarFormu")
 
 let selectAccion = document.getElementById("accion")
+let mensaje
 
 selectAccion.onchange = () => {
     switch (selectAccion.value) {
@@ -50,4 +52,33 @@ selectAccion.onchange = () => {
         default:
             break;
     }
+}
+
+
+btnSubmit.onclick = (e) => {
+    clearInputs()
+    e.preventDefault()
+    switch (selectAccion.value) {
+        case "transferencia":
+            mensaje = "Transferencia realizada. Le enviareos por correo el comprobante"
+            break;
+        case "prestamo":
+            mensaje = "Solicitud de prestamo realizada. En los proximos dias nos estaremos comunicando con usted para contirnuar con el proceso"
+            break;
+        case "pago":
+            mensaje = "Pago de servicio realizado. Gracias por elegirnos!"
+            break;
+        default:
+            break;
+    }
+    window.alert(mensaje)
+}
+
+
+function clearInputs() {
+    document.getElementById("cbu").value =""
+    document.getElementById("monto-transferencia").value =""
+    document.getElementById("codigo-pago").value =""
+    document.getElementById("monto-prestamo").value =""
+    document.getElementById("plazo").value =""
 }
