e540d75914161ed6b8e6c04007dcd2f56a57bfb3
Vanpa99 <mativanpa99@gmail.com>
2024-08-31 23:51:01 -0300

    Transferencias

--- Js/check.js
+++ Js/check.js
@@ -40,3 +40,65 @@ function acceder(event) {
         alert("Credenciales Incorrectas.");
     }
 }
+
+//TRANSFERENCIAS 
+
+const contenidoPago = document.querySelector(".input-pago")
+const contenidoPrestamo = document.querySelector(".input-prestamo")
+const contenidoTransferencia = document.querySelector(".input-transferencia")
+const btnSubmit = document.querySelector("#enviarFormu")
+
+let selectAccion = document.getElementById("accion")
+let mensaje
+
+selectAccion.onchange = () => {
+    switch (selectAccion.value) {
+        case "transferencia":
+            contenidoTransferencia.style.display = "block"
+            contenidoPrestamo.style.display = "none"
+            contenidoPago.style.display = "none"
+            break;
+        case "prestamo":
+            contenidoTransferencia.style.display = "none"
+            contenidoPrestamo.style.display = "block"
+            contenidoPago.style.display = "none"
+            break;
+        case "pago":
+            contenidoTransferencia.style.display = "none"
+            contenidoPrestamo.style.display = "none"
+            contenidoPago.style.display = "block"
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+btnSubmit.onclick = (e) => {
+    clearInputs()
+    e.preventDefault()
+    switch (selectAccion.value) {
+        case "transferencia":
+            mensaje = "Transferencia realizada. Le enviareos por correo el comprobante"
+            break;
+        case "prestamo":
+            mensaje = "Solicitud de prestamo realizada. En los proximos dias nos estaremos comunicando con usted para contirnuar con el proceso"
+            break;
+        case "pago":
+            mensaje = "Pago de servicio realizado. Gracias por elegirnos!"
+            break;
+        default:
+            break;
+    }
+    window.alert(mensaje)
+}
+
+
+function clearInputs() {
+    document.getElementById("cbu").value =""
+    document.getElementById("monto-transferencia").value =""
+    document.getElementById("codigo-pago").value =""
+    document.getElementById("monto-prestamo").value =""
+    document.getElementById("plazo").value =""
+}
