dfebaca68249dabfbc2224dbaf0a6cb8c319e80e
Vanpa99 <mativanpa99@gmail.com>
2024-09-02 18:05:05 -0300

    anda todo

--- Js/check.js
+++ Js/check.js
@@ -156,7 +156,6 @@ function acceder(event) {
 //TRANSFERENCIAS 
 
 const contenidoPago = document.querySelector(".input-pago")
-const contenidoPrestamo = document.querySelector(".input-prestamo")
 const contenidoTransferencia = document.querySelector(".input-transferencia")
 const btnSubmit = document.querySelector("#enviarFormu")
 
@@ -167,17 +166,10 @@ selectAccion.onchange = () => {
     switch (selectAccion.value) {
         case "transferencia":
             contenidoTransferencia.style.display = "block"
-            contenidoPrestamo.style.display = "none"
-            contenidoPago.style.display = "none"
-            break;
-        case "prestamo":
-            contenidoTransferencia.style.display = "none"
-            contenidoPrestamo.style.display = "block"
             contenidoPago.style.display = "none"
             break;
         case "pago":
             contenidoTransferencia.style.display = "none"
-            contenidoPrestamo.style.display = "none"
             contenidoPago.style.display = "block"
             break;
     
@@ -194,9 +186,6 @@ btnSubmit.onclick = (e) => {
         case "transferencia":
             mensaje = "Transferencia realizada. Le enviareos por correo el comprobante"
             break;
-        case "prestamo":
-            mensaje = "Solicitud de prestamo realizada. En los proximos dias nos estaremos comunicando con usted para contirnuar con el proceso"
-            break;
         case "pago":
             mensaje = "Pago de servicio realizado. Gracias por elegirnos!"
             break;
@@ -211,6 +200,4 @@ function clearInputs() {
     document.getElementById("cbu").value =""
     document.getElementById("monto-transferencia").value =""
     document.getElementById("codigo-pago").value =""
-    document.getElementById("monto-prestamo").value =""
-    document.getElementById("plazo").value =""
 }
