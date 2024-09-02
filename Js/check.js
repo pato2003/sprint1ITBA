165bf0f3e9049de2383f1d3b8b046a87ac735311
Vanpa99 <mativanpa99@gmail.com>
2024-09-02 17:37:56 -0300

    algunos cambios

--- Js/check.js
+++ Js/check.js
@@ -156,7 +156,7 @@ function acceder(event) {
 //TRANSFERENCIAS 
 
 const contenidoPago = document.querySelector(".input-pago")
-const contenidoPrestamo = document.querySelector(".input-prestamo")
+// const contenidoPrestamo = document.querySelector(".input-prestamo")
 const contenidoTransferencia = document.querySelector(".input-transferencia")
 const btnSubmit = document.querySelector("#enviarFormu")
 
@@ -170,11 +170,11 @@ selectAccion.onchange = () => {
             contenidoPrestamo.style.display = "none"
             contenidoPago.style.display = "none"
             break;
-        case "prestamo":
-            contenidoTransferencia.style.display = "none"
-            contenidoPrestamo.style.display = "block"
-            contenidoPago.style.display = "none"
-            break;
+        // case "prestamo":
+        //     contenidoTransferencia.style.display = "none"
+        //     contenidoPrestamo.style.display = "block"
+        //     contenidoPago.style.display = "none"
+        //     break;
         case "pago":
             contenidoTransferencia.style.display = "none"
             contenidoPrestamo.style.display = "none"
@@ -194,9 +194,9 @@ btnSubmit.onclick = (e) => {
         case "transferencia":
             mensaje = "Transferencia realizada. Le enviareos por correo el comprobante"
             break;
-        case "prestamo":
-            mensaje = "Solicitud de prestamo realizada. En los proximos dias nos estaremos comunicando con usted para contirnuar con el proceso"
-            break;
+        // case "prestamo":
+        //     mensaje = "Solicitud de prestamo realizada. En los proximos dias nos estaremos comunicando con usted para contirnuar con el proceso"
+        //     break;
         case "pago":
             mensaje = "Pago de servicio realizado. Gracias por elegirnos!"
             break;
